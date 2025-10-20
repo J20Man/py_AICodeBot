d8bd7dc4afac9ddbac6a2dd0661d49c9120ccc48
J20Man <josephtwentyman@hotmail.co.uk>
2025-10-20 21:56:35 +0100

    Adding API Code, and checking token usage etc.

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+    
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    response_tokens = response.usage_metadata.candidates_token_count
+    prompt_tokens = response.usage_metadata.prompt_token_count
+
+    print(f"{response.text} \n Prompt tokens: {prompt_tokens} \n Response tokens: {response_tokens}")
+
+
+if __name__ == "__main__":
+    main()
